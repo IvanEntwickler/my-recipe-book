cad082bd41593ff3390ed2d6820ef2ad92988757
IvanEntwickler <53708215+IvanEntwickler@users.noreply.github.com>
2020-03-28 00:13:17 +0100

    worked on header added scss, also created recipes model to output data into the recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test', 'Test2', 'https://image.shutterstock.com/image-photo/colorful-flower-on-dark-tropical-260nw-721703848.jpg')
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClickRecipe() {
+    return this.recipes;
   }
 
 }
